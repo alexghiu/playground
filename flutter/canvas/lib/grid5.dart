eb809ffdb0ced0333608b8b204ffe87f8731596a
Alex Ghiu <alex.ghiu@gmail.com>
2020-07-22 20:08:17 +0200

    Cleanup

--- flutter/canvas/lib/grid5.dart
+++ flutter/canvas/lib/grid5.dart
@@ -1,28 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Grid5 extends StatelessWidget {
-  /*
-  @override
-  Widget build(BuildContext context) {
-    final image = DecorationImage(
-      repeat: ImageRepeat.repeat,
-      image: AssetImage('assets/tile.png'),
-    );
-    return InteractiveViewer(
-      minScale: 0.1,
-      maxScale: 1.6,
-      child: SizedBox(
-        width: 800,
-        height: 1000,
-        child: DecoratedBox(
-            decoration: BoxDecoration(
-          image: image,
-        )),
-      ),
-    );
-  }
-  */
-
   @override
   Widget build(BuildContext context) {
     final image = DecorationImage(
@@ -35,8 +13,8 @@ class Grid5 extends StatelessWidget {
         boundaryMargin: EdgeInsets.all(20.0),
         constrained: false,
         child: SizedBox(
-          width: 800,
-          height: 1000,
+          width: 2000,
+          height: 4000,
           child: DecoratedBox(
             decoration: BoxDecoration(
               image: image,
