702385f071690de40eb1888d4d336ebfd1a0e4b1
Alex Ghiu <alex.ghiu@gmail.com>
2020-07-22 15:11:01 +0200

    Cleanup

--- flutter/canvas/lib/grid3.dart
+++ flutter/canvas/lib/grid3.dart
@@ -4,22 +4,6 @@ import 'package:flutter/material.dart';
 
 import 'dart:developer';
 
-/*
-class Grid3 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final recorder = PictureRecorder();
-    final canvas = Canvas(recorder);
-    canvas.drawRect(
-      Rect.fromLTWH(5, 5, 1, 1),
-      Paint()..color = Colors.red[400],
-    );
-    final picture = recorder.endRecording();
-    final image = picture.toImage(10, 10);
-  }
-}
-*/
-
 class Grid3 extends StatefulWidget {
   _Grid3State createState() => _Grid3State();
 }
@@ -30,12 +14,10 @@ class _Grid3State extends State<Grid3> {
   @override
   void initState() {
     super.initState();
-    print('_Grid3State.initState');
     _prepareBackground();
   }
 
   Future<void> _prepareBackground() async {
-    print('_Grid3State._prepareBackground');
     final side = 10.0;
     final weight = 1.0;
     final recorder = PictureRecorder();
@@ -47,20 +29,16 @@ class _Grid3State extends State<Grid3> {
     final picture = recorder.endRecording();
     final image = await picture.toImage(side.toInt(), side.toInt());
     final bd = await image.toByteData(format: ImageByteFormat.png);
-    print(_image);
-    print(_image == null);
-    print(_image != null);
     setState(() {
       _image = MemoryImage(Uint8List.view(bd.buffer));
-      print('_Grid3State._prepareBackground.after');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: 100,
-      height: 300,
+      width: 1000,
+      height: 800,
       child: DecoratedBox(
         decoration: BoxDecoration(
           // color: _image == null ? Colors.yellow[300] : Colors.blue[300],
