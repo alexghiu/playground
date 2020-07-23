f6117e5119d47d219a94f5f5ea44c7fe9b87ed3f
Alex Ghiu <alex.ghiu@gmail.com>
2020-07-23 09:49:17 +0200

    Grid border, larger margin and new tile

--- flutter/canvas/lib/grid5.dart
+++ flutter/canvas/lib/grid5.dart
@@ -4,13 +4,14 @@ class Grid5 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final image = DecorationImage(
+      alignment: Alignment.topLeft,
       repeat: ImageRepeat.repeat,
       image: AssetImage('assets/tile.png'),
     );
 
     return Center(
       child: InteractiveViewer(
-        boundaryMargin: EdgeInsets.all(20.0),
+        boundaryMargin: EdgeInsets.all(40.0),
         constrained: false,
         child: SizedBox(
           width: 2000,
@@ -18,6 +19,9 @@ class Grid5 extends StatelessWidget {
           child: DecoratedBox(
             decoration: BoxDecoration(
               image: image,
+              border: Border.all(
+                color: Colors.grey[200],
+              ),
             ),
             child: Center(
               child: Container(
